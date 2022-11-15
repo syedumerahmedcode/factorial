c20a20b9e70f474bed75eed2caf2340ffce5e602
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2022-11-15 19:55:16 +0100

    Feature: Implemented the logic for CapitalzeWordService.

--- src/com/umer/capitalizeword/CapitalizwWordService.java
+++ src/com/umer/capitalizeword/CapitalizwWordService.java
@@ -2,6 +2,17 @@ package com.umer.capitalizeword;
 
 public class CapitalizwWordService {
 	public String capitalizeWord(String str){
-		return "incorrect string";
+		 
+	    if(str.isEmpty()) {
+	        return str;
+	    }
+	    char chr = str.charAt(str.length()-1);
+	    if(str.length()==1) {
+	        return Character.toString(Character.toUpperCase(chr));
+	    }
+	    if((str.substring(str.length()-2, str.length()-1).equals(" "))) {
+	        chr = Character.toUpperCase(chr);
+	    }
+	    return capitalizeWord(str.substring(0,str.length()-1))+ Character.toString(chr);
 	}
 }
